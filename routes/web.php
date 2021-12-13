ad0c3bbd1ebda1c0460aac6fcf76cce6999783c7
Yaroslav <ray.last.light@gmail.com>
2021-12-14 00:26:01 +0300

    Add posts, tags, categories and authorization routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\TagsController;
 use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\Admin\PostsController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,14 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/post/{slug}', [HomeController::class, 'show'])->name('post.show');
+Route::get('/tag/{slug}', [HomeController::class, 'tag'])->name('tag.show');
+Route::get('/category/{slug}', [HomeController::class, 'category'])->name('category.show');
+Route::get('/register', [AuthController::class, 'showRegisterForm']);
+Route::post('/register', [AuthController::class, 'registration']);
+Route::get('/login', [AuthController::class, 'showLoginForm']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::group(['prefix' => 'admin'], function (){
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
