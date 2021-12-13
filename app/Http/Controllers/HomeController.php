bdcc7dd5d4716b8b3d6a76b1385a9a5ddde849f2
Yaroslav <ray.last.light@gmail.com>
2021-12-14 00:36:06 +0300

    Individual post page. Category and tag pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::simplePaginate(3);
-        return view('pages.index', compact('posts'));
+        $posts = Post::paginate(3);
+
+
+        return view('pages.index', [
+            'posts' => $posts,
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('pages.post', compact('post'));
+    }
+
+    public function tag($slug)
+    {
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+        $posts = $tag->posts()->paginate(3);
+
+        return view('pages.list', compact('posts'));
+    }
+
+    public function category($slug)
+    {
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->paginate(3);
+
+        return view('pages.list', compact('posts'));
     }
 }
