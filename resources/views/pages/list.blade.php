28caa13e12d266eb5cc04e01f14db5728ee8ccb6
Yaroslav <ray.last.light@gmail.com>
2021-12-15 01:40:49 +0300

    Minor changes

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -8,30 +8,30 @@
                 <div class="col-md-8">
                     <div class="row">
                         @foreach($posts as $post)
-                            <div class="col-md-6">
-                            <article class="post post-grid">
-                                <div class="post-thumb">
-                                    <a href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" alt=""></a>
-                                    <a href="{{route('post.show', $post->slug)}}" class="post-thumb-overlay text-center">
-                                        <div class="text-uppercase text-center">View Post</div>
-                                    </a>
-                                </div>
-                                <div class="post-content">
-                                    <header class="entry-header text-center text-uppercase">
-                                        @if($post->hasCategory())
-                                            <h6><a href="{{route('category.show', $post->category->slug)}}">{{$post->getCategoryTitle()}}</a></h6>
-                                        @endif
+                            <div class="col-md-6" style="height: 500px">
+                                <article class="post post-grid">
+                                    <div class="post-thumb">
+                                        <a href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" alt=""></a>
+                                        <a href="{{route('post.show', $post->slug)}}" class="post-thumb-overlay text-center">
+                                            <div class="text-uppercase text-center">View Post</div>
+                                        </a>
+                                    </div>
+                                    <div class="post-content">
+                                        <header class="entry-header text-center text-uppercase">
+                                            @if($post->hasCategory())
+                                                <h6><a href="{{route('category.show', $post->category->slug)}}">{{$post->getCategoryTitle()}}</a></h6>
+                                            @endif
 
-                                        <h1 class="entry-title"><a href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h1>
-                                    </header>
-                                    <div class="entry-content">
-                                        {!! $post->description !!}
+                                            <h1 class="entry-title"><a href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h1>
+                                        </header>
+                                        <div class="entry-content">
+                                            {!! $post->description !!}
 
-                                        <div class="social-share">
-                                            <span class="social-share-title pull-left text-capitalize">By Rubel On {{$post->getDate()}}</span>
+                                            <div class="social-share">
+                                                <span class="social-share-title pull-left text-capitalize">By Rubel On {{$post->getDate()}}</span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
                             </article>
                         </div>
                         @endforeach
